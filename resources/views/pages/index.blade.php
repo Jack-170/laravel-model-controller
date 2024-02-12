0d546c903f96e463346b2c737ec1f97677c57398
Jack-170 <agavejack@hotmail>
2024-02-12 18:35:57 +0100

    add movies card

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.main-layout')
 @section('head')
-    <title>Home</title>
+    <title>Movies</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+<div class="row">
+    @foreach ($movies as $movie)
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">{{ $movie['title'] }}</h5>
+                    <p class="card-text">Titolo Originale: {{ $movie['original_title'] }}</p>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Nazionalità: {{ $movie['nationality'] }}</p>
+                    <p class="card-text">Data: {{ $movie['date'] }}</p>
+                    <p class="card-text">Voto: {{ $movie['vote'] }}</p>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
 @endsection
